2e50a408a51fafc9b0e9501b494ef4c249212a9c
jin song <songjin3914@gmail.com>
2024-04-06 12:53:27 +0900

    옵션메뉴

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -7,12 +11,52 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
+
+
+
 class _MainScreenState extends State<MainScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text('나만의 웹프라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value){
+              print(value);
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem<String>(
+                value: 'https://www.google.com',
+                child: Text('구글'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.naver.com',
+                child: Text('네이버'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.kakao.com',
+                child: Text('카카오'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
       ),
     );
   }
